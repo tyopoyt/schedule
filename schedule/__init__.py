f5ff213acef8ceeab6affc87b55914ec319a9ea0
David Crouse <dcrouseIII@hotmail.com>
2021-01-21 22:22:13 -0500

    Print

--- schedule/__init__.py
+++ schedule/__init__.py
@@ -472,8 +472,9 @@ class Job(object):
         :param job_func: The function to be scheduled
         :return: The invoked job instance
         """
-        print('IN THE DO')
+
         self.job_func = functools.partial(job_func, *args, **kwargs)
+        print('IN THE DO', self.job_func)
         functools.update_wrapper(self.job_func, job_func)
         self._schedule_next_run()
         self.scheduler.jobs.append(self)
